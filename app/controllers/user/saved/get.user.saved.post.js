3aa66dff3a7926c58bdfedd145e0dfb64c109ae7
alex <alex@areax.ai>
2025-07-04 09:47:59 +0000

    created method to get user save posts

---
+++ app/controllers/user/saved/get.user.saved.post.js
@@ -0,0 +1,25 @@
+const { db2 } = require("../../../models");
+
+const SavedPosts = db2.saved_posts;;
+
+module.exports.getUserSavedPosts = async (email,reference_number) => {
+  try {
+    const count = await SavedPost.count({where:{email: email,reference_number: reference_number}});
+    if(count > 0){
+       const record = await SavedPost.findAll({
+           attributes:['post_id','like_id','user_id','email','reference_number'],
+           where:{
+               email: email,
+               reference_number: reference_number,
+           },
+       });
+       return [true,record,`List of Saved Post[s]`];
+    }else{
+       return [false,count,`No Saved Post[s]`];
+    }
+
+  } catch(error) {
+    console.error('Error fetching user saved post[s]:', error);
+    return [false,error?.message];
+  }
+};
